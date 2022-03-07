a9edacc612ab6f7c005159726c4d451af64dc37e
Jeremy Stiff <jstiff@ggc.edu>
2022-03-06 23:23:45 -0500

    Whole lot of nothinng progress. Arrays still are undefined while having data avaliable. Promises and async is cancer.

--- src/app/jeremy-testing/jeremy-testing.component.ts
+++ src/app/jeremy-testing/jeremy-testing.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataSnapshot } from 'firebase/database';
+
+//Needed import to use the Firebase service to get the data
 import { FirebaseService } from '../services/firebase.service';
 
 @Component({
@@ -12,15 +14,25 @@ export class JeremyTestingComponent implements OnInit {
   constructor(private firebaseservice: FirebaseService) { }
 
   item_array: DataSnapshot[] = [];
+  key_array: Array<string> = []
+  val_array: any[] = [];
+  ele_array = [];
+  test = [];
 
 
   //Author: Jeremy Stiff jstiff@ggc.edu
   //Using the ngOnInit() method to get the data when the component is loaded
   //The array contains the items that have item.key and item.val()
   ngOnInit() {
-    console.log('The console works!');
-    this.item_array = this.firebaseservice.getData();
-    console.log(this.item_array);
+    //this.item_array = this.firebaseservice.getData();
+    this.key_array = this.firebaseservice.getKeys();
+    //this.firebaseservice.getSnapshot();
+    this.val_array = this.firebaseservice.getVals();
+    //this.test = this.firebaseservice.getTest();
+    //console.log(this.item_array);
+    console.log(this.key_array);
+    //console.log(this.snapshot);
+    console.log(this.val_array);
   }
 
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, set, get, child, DataSnapshot } from 'firebase/database';
-import { Data } from '@angular/router';
+import { getDatabase, ref, get, child, DataSnapshot } from 'firebase/database';
 
 const app = initializeApp(environment.firebase);
 const database = getDatabase(app);
@@ -12,19 +11,19 @@ const dbRef = ref(database);
   providedIn: 'root',
 })
 export class FirebaseService {
-  constructor() {}
+  constructor() { }
 
   //Author: Jeremy Stiff jstiff@ggc.edu
   //Method returns an array containing the antibiotic objects with bacterium childrem
   //Accessing the antibioic name is done through: item.key
   //Accessing the bacterium children is done through: item.val()
-  getData():DataSnapshot[] {
-    var item_array:DataSnapshot[] = [];
+  getData(): DataSnapshot[] {
+    var item_array: DataSnapshot[] = [];
     get(child(dbRef, '/')).then((snapshot) => {
       if (snapshot.exists()) {
-        console.log(snapshot.forEach((item) => {
+        snapshot.forEach((item) => {
           item_array.push(item);
-        }));
+        });
       } else {
         console.log('No data avaliable');
       }
@@ -32,5 +31,38 @@ export class FirebaseService {
       console.error(error);
     });
     return item_array;
+  }
+
+  getKeys(): Array<string> {
+    var key_array: string[] = [];
+    get(child(dbRef, '/')).then((snapshot) => {
+      if (snapshot.exists()) {
+        for (var x in snapshot.val()) {
+          key_array.push(x);
+        }
+      } else {
+        console.log('No data avaliable');
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+    return key_array;
+  }
+
+  getVals(): any[] {
+    var val_array: any[] = []
+    get(child(dbRef, '/')).then((snapshot) => {
+      if (snapshot.exists()) {
+        for (var x in snapshot.val()) {
+          val_array.push(snapshot.val()[x]);
+        }
+        return val_array;
+      } else {
+        console.log('No data avaliable');
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+    return val_array;
   }
 }
