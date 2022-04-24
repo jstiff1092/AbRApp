290ed9232d95d6eeb856aa7f66e533f9123de1b8
Jeremy Stiff <jstiff@ggc.edu>
2022-04-24 19:19:57 -0400

    help

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/www'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/src/index.html'));
+res.sendFile(path.join(__dirname+'/www/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
