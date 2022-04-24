283368400019f7d3dba865af153827ac66c20a9a
Jeremy Stiff <jstiff@ggc.edu>
2022-04-24 00:02:22 -0400

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCHkJBIrAvYEJBNZC1rDEnETLghDFRuNR8',
+    authDomain: 'antibiotic-resistance-app.firebaseapp.com',
+    databaseURL:
+      'https://antibiotic-resistance-app-default-rtdb.firebaseio.com',
+    projectId: 'antibiotic-resistance-app',
+    storageBucket: 'antibiotic-resistance-app.appspot.com',
+    messagingSenderId: '965395585433',
+    appId: '1:965395585433:web:7ff21515ab81cb69775b08',
+  }
 };
