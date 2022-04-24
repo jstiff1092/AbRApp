bc2a4a09107758d68351a1b44eb04a4f29b9274a
Jeremy Stiff <jstiff@ggc.edu>
2022-04-24 00:38:48 -0400

    :(

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/AbRApp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/AbRApp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/AbRApp/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
