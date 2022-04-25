2c22d342dc72066162553362c500d5f61aa4d202
SomecallmeBrian23 <92411307+SomecallmeBrian23@users.noreply.github.com>
2022-04-24 20:05:00 -0400

    Trying to implement the search function

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterEvent } from '@angular/router';
+import { FirebaseService } from '../services/firebase.service';
+import { Antibiotic } from '../shared/Antibiotic';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage{
+  public antiList: any;
+  constructor(private firebase: FirebaseService) {}
 
-  constructor() {}
+  ngOnInIt(){
+  }
 
+  async initializeItems(): Promise<any> {
+    const antiItem = await this.initializeItems();
+    return this.antiList;
+  }
+
+  async filterList(evt) {
+    this.antiList = await this.initializeItems();
+    const searchTerm = evt.srcElement.value;
+
+    if (!searchTerm){
+      return;
+    }
+
+    this.antiList = this.antiList.filterList(currentAnti => {
+      if (currentAnti.name && searchTerm){
+        return (currentAnti.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1 ||
+        currentAnti.type.toLowerCase().indexOf(searchTerm.toLowerCase()));
+      }
+    });
+  }
 }
